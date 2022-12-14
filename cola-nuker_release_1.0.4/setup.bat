bc32f33d76c5c37663d741bb4882f9a4942a6715
Blizz / Luya <109612852+LuyaTools@users.noreply.github.com>
2022-12-14 20:05:17 +0100

    Update setup.bat

--- cola-nuker_release_1.0.4/setup.bat
+++ cola-nuker_release_1.0.4/setup.bat
@@ -1,10 +1,21 @@
 color c
 title COLA-NUKER Setup
+cls
 pip install customtkinter
+cls
+pip install colorama
 cls
 pip install discord-webhook
 cls
-pip install tkinter
+pip install pysmartthings
+cls
+pip install aiohttp
+cls
+pip install aioconsole
+cls
+pip install asyncio
+cls
+pip install sll
 cls
 pip install nextcord
 cls
